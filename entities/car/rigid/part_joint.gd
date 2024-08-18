25a1d74f6465dd3818a3f29ed9fbeb77e655bb33
Brutenis <brutenis@gmail.com>
2024-08-18 13:31:02 +0200

    change car building to flat structure

--- entities/car/rigid/part_joint.gd
+++ entities/car/rigid/part_joint.gd
@@ -7,21 +7,23 @@ const POWER = 500_000
 
 var target_diff
 
-func _ready():
-	calibrate()
+#func _ready():
+#	calibrate()
 
 func calibrate():
 	a = get_node(node_a)
 	b = get_node(node_b)
-	target_diff = a.rotation - b.rotation
+	if a != null and b != null:
+		target_diff = a.rotation - b.rotation
 
 func sigmoid(x) -> float:
 	return 2 / (1 + exp(-x)) - 1
 
 func _physics_process(delta):
-	var angle = a.rotation - b.rotation
-	var diff = target_diff - angle
-	
-	var torque = POWER * rad_to_deg(sigmoid(diff))
-	print(diff, "\t", torque)
-	a.apply_torque(torque)
+	if a != null and b != null:
+		var angle = a.rotation - b.rotation
+		var diff = target_diff - angle
+		
+		var torque = POWER * rad_to_deg(sigmoid(diff))
+		#print(diff, "\t", torque)
+		#a.apply_torque(torque)
